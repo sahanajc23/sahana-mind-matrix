6deac233f4cab34f595ef983fec7c1eef4d293d9
sahanajc23 <vinya8680@gmail.com>
2026-05-19 08:34:05 +0530

    Add files via upload

---
+++ build.gradle.kts
@@ -0,0 +1,5 @@
+// Top-level build file where you can add configuration options common to all sub-projects/modules.
+plugins {
+    id("com.google.devtools.ksp") version "2.1.20-1.0.32" apply false
+    alias(libs.plugins.android.application) apply false
+}
